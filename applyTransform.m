deaf0fc148e9a00a92da35c5fa209fada025758d
lusteven901228 <lusteven901228@gmail.com>
2024-11-25 08:48:16 +0800

    initial commit, mostly finished except presets

---
+++ applyTransform.m
@@ -0,0 +1,27 @@
+function [outputMusic] = applyTransform(inputMusic,inputMap)
+% this can be simply divided into 4 steps
+% 1. split the channels
+[~, numChannels] = size(inputMusic);
+
+% initialize output
+outputMusic = zeros(size(inputMusic));
+
+% pre-calculate the multiplier
+inputMapMultiplier = 10.^(inputMap/20);
+
+% Apply the transformation for each channel
+for i = 1:numChannels
+    channel = inputMusic(:, i);
+    fftMusic = fft(channel);
+    fftFiltered = fftMusic .* inputMapMultiplier(:);
+    transformedChannel = ifft(fftFiltered,"symmetric");
+
+    % Normalize the transformed signal to prevent clipping
+    maxVal = max(abs(transformedChannel));
+    if maxVal > 0
+        transformedChannel = transformedChannel / maxVal; % Scale so that max value is 1
+    end
+    outputMusic(:, i) = transformedChannel;
+end
+end
+
